e1756e73c18827083a97f56ee17a09a828808eb5
Natalie Gallo <nataliegallo@Natalies-Air.fios-router.home>
2022-12-12 17:55:37 -0500

    finished task c

--- funcs.cpp
+++ funcs.cpp
@@ -20,3 +20,12 @@ int sumRange(int left, int right){
     
     return left + sumRange(left + 1, right);
 }
+
+//TASK C
+int sumArray(int *arr, int size){
+    if (size < 0){
+        return 0;
+    }
+    //start adding from last element and then decrement the size by 1
+    return *(arr + (size - 1)) + sumArray(arr, size - 1); // 1. size = 10; 10 - 1 = [9] + (SIZE = 10 - 1 = 9)    2. size = 9; 9-1 = [8] + (SIZE = 8-1 = 7)
+}
--- main.cpp
+++ main.cpp
@@ -10,12 +10,35 @@ int main()
     cout << result << endl;
 
     //TASK B
-    cout << "TASK B\n---------------------------\n\n";
+    cout << "\nTASK B\n---------------------------\n\n";
     int x = sumRange(1, 3);
     cout << "This is " << x << endl;   // 6
     
     int y = sumRange(-2, 10);
     cout << "That is " << y << endl;   // 52
+
+    //TASK C
+    cout << "\nTASK C\n---------------------------\n\n";
+    int size = 10;
+    int *arr = new int[size]; // allocate array dynamically
+    arr[0] = 12;
+    arr[1] = 17;
+    arr[2] = -5;
+    arr[3] = 3;
+    arr[4] = 7;
+    arr[5] = -15;
+    arr[6] = 27;
+    arr[7] = 5;
+    arr[8] = 13;
+    arr[9] = -21;
+
+    int sum1 = sumArray(arr, size); // Add all elements
+    cout << "Sum is " << sum1 << endl;  // Sum is 43
+    
+    int sum2 = sumArray(arr, 5); // Add up first five elements
+    cout << "Sum is " << sum2 << endl;  // Sum is 34
+
+    delete[] arr;         // deallocate it
 
     return 0;
 }
